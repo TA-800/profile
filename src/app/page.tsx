c7fe500e4231e0da9939d8b3245334611912b42b
TA-800 <taherali0905@gmail.com>
2023-06-04 21:28:46 +0300

    final touches 2 (links)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -401,8 +401,7 @@ function ContactForm({ marginBottom }: { marginBottom?: number }) {
         once: true,
         margin: `0px 0px ${marginBottom ?? -75}px 0px`,
     });
-    // 0 -> not submitted, 1 -> submitting, 2 -> submitted, 3 -> web3form email service error, 4 -> fetch request error
-    const [submitted, setSubmitted] = useState("blank");
+    const [submitted, setSubmitted] = useState<"blank" | "submitting" | "success" | "error">("blank");
 
     function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -481,7 +480,7 @@ function ContactForm({ marginBottom }: { marginBottom?: number }) {
                     <Button
                         disabled={submitted !== "blank"}
                         type="submit"
-                        className={submitted === "submitted" ? "!text-green-400" : submitted === "error" ? "!text-red-400" : ""}>
+                        className={submitted === "success" ? "!text-green-400" : submitted === "error" ? "!text-red-400" : ""}>
                         {/* Leading icons */}
                         {submitted === "blank" && (
                             <svg
@@ -749,6 +748,7 @@ function CardCarousel() {
             w-full
             border-y-4 border-y-white/5`}>
             <Card
+                gitLink="https://github.com/TA-800/Erudition"
                 imgSrc={eruditionPic}
                 link="https://erudition.up.railway.app/"
                 title="Erudition"
@@ -757,6 +757,7 @@ function CardCarousel() {
                 Web app for students to manage study materials and assignments, with Django backend.
             </Card>
             <Card
+                gitLink="https://github.com/TA-800/rtc-app"
                 imgSrc={rtcappPic}
                 link="https://rtcomms.vercel.app/"
                 title="RTC Chatapp"
@@ -765,6 +766,7 @@ function CardCarousel() {
                 Real-time chat web app built with React and Supabase.
             </Card>
             <Card
+                gitLink="https://github.com/TA-800/Mouse-Magnet-Style-Chasing-Jutsu"
                 imgSrc={mousemagnetPic}
                 link="https://magnetic-mouse.vercel.app/"
                 title="Mouse Magnet"
@@ -772,10 +774,20 @@ function CardCarousel() {
                 onClick={() => setActiveCard(3)}>
                 Magnetic mouse and buttons that follow the cursor pleasantly.
             </Card>
-            <Card imgSrc={wordlePic} title="Wordle Help" isactive={activeCard === 4} onClick={() => setActiveCard(4)}>
+            <Card
+                gitLink="https://github.com/TA-800/Wordle-Assistant"
+                imgSrc={wordlePic}
+                title="Wordle Help"
+                isactive={activeCard === 4}
+                onClick={() => setActiveCard(4)}>
                 A Python bot to help you solve Wordle puzzles.
             </Card>
-            <Card imgSrc={mousemagnetPic} title="Discord Bot" isactive={activeCard === 5} onClick={() => setActiveCard(5)}>
+            <Card
+                gitLink="https://github.com/TA-800/Techio-DiscordBot-"
+                imgSrc={mousemagnetPic}
+                title="Discord Bot"
+                isactive={activeCard === 5}
+                onClick={() => setActiveCard(5)}>
                 An old Discord Python bot with many random features - word game, fetch YT vids, Reddit memes, etc.
             </Card>
         </div>
@@ -787,6 +799,7 @@ function Card({
     title,
     isactive,
     link,
+    gitLink,
     onClick,
     children,
 }: {
@@ -794,6 +807,7 @@ function Card({
     title: string;
     isactive: boolean;
     link?: string;
+    gitLink?: string;
     onClick?: () => void;
     children: React.ReactNode;
 }) {
@@ -882,7 +896,12 @@ function Card({
                     }}>
                     <div className="overflow-hidden p-2 flex flex-row gap-2">
                         {link && (
-                            <button className="btn btn-primary !rounded-full lg:!p-2 !p-1">
+                            <button
+                                onClick={() => {
+                                    // Go to link
+                                    if (link) window.open(link, `${title} live link`);
+                                }}
+                                className="btn btn-primary !rounded-full lg:!p-2 !p-1">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
@@ -898,7 +917,13 @@ function Card({
                                 </svg>
                             </button>
                         )}
-                        <button className="btn btn-secondary !rounded-full lg:!p-2 !p-1">
+                        {/* Github button */}
+                        <button
+                            onClick={() => {
+                                // Go to github link
+                                if (gitLink) window.open(gitLink, `${title} GitHub link`);
+                            }}
+                            className="btn btn-secondary !rounded-full lg:!p-2 !p-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#f3f4f6">
                                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                             </svg>
